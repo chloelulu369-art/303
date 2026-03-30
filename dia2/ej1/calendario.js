f0d98d2235bb7d98d7b6d6b82ed92a7ae4837f8f
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-03-30 17:33:00 -0300

    tarea dia uno precensial

---
+++ dia2/ej1/calendario.js
@@ -0,0 +1,6 @@
+
+const meses = [enero, febrero, marzo, abril, mayo, junio, julio, agosto, septiembre, octubre, noviembre, diciembre]
+console.log(meses);
+meses.forEach(mes => {
+  console.log(mes);
+});
---
+++ dia2/ej2/2.js
@@ -0,0 +1,4 @@
+<script>
+    var valores = [true, 5, false, "hola", "adios", 2];
+    var valores = new Boolean(false);
+</script>
