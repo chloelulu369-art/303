efd39a35e362ed5ccd9f139962733885a2a55c1b
Thiago-Anzaldo <thiagoanzaldo008@gmail.com>
2026-04-06 17:50:18 -0300

    6-4 ejerccio 4,5 y 6

--- dia2/ej4/dni.js
+++ dia2/ej4/dni.js
@@ -1,2 +1,19 @@
-var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
-'E', 'T'];
+<script>
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+    var numero;
+    numero = prompt("Ingrese un número de DNI");
+    if(isNaN(numero) || numero==null || numero==""){
+        document.writeln("Valor inválido en la posición: ", i, "° <br>")}
+ 
+    if(numero =< 0 || numero =< 99999999){
+        //calcula la letra que le corresponde según el método explicado anteriormente.
+    }
+
+    for(let j=0; j=23 ; j++)
+    {
+        if(nf == letras(j)){
+        document.writeln("Correcto, siga avanzando")}
+        else{
+        document.writeln("error,reintente") } 
+    }
+</script>
---
+++ dia2/ej5/factorial.js
@@ -0,0 +1,10 @@
+<script>
+    var numero, resultado;
+    numero = prompt("Ingrese un número a factorizar");
+
+    for(let j==0; j == numero ; j++)
+    {
+       resultado == numero.(numero -j)
+    }
+
+</script>
